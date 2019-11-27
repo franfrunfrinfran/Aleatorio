1d5149021c440af60a2a06364928779378906b35
franfrunfrinfran <javierufo91@gmail.com>
2019-11-27 09:43:17 +0100

    Aleatorio

---
+++ src/es/studium/aleatorio/Aleatorios.java
@@ -0,0 +1,10 @@
+package es.studium.aleatorio;
+
+public class Aleatorios {
+	public static void main(String[]args) {
+		
+	
+	System.out.println("Número aleatorio " + Math.random());
+	System.out.println("Otro número aleatorio " + Math.random());
+	}
+}
